9710d27a133c6b8874dc5d5b6b0dcc7559c82d6a
Manel.Defnoune <36042435+manel009@users.noreply.github.com>
2021-09-05 12:31:30 +0200

    boutton close fonctionnel

--- Game-on/modal.js
+++ Game-on/modal.js
@@ -1,14 +1,16 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
+// on selectionne l'élément qui a la class bground et on le met dans modalbg
 const modalbg = document.querySelector(".bground");
+// on selectionne tous les éléments qui ont la class modal-btn
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
@@ -17,7 +19,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
-
+// Fonction fermeture formulaire
+function closeForm() {
+    modalbg.style.display = "none";
+}
